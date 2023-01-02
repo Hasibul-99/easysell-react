9146c5c9bb6e53a6dd05838611567031d554bd39
hasibul-99 <hasibul_uap@outlook.com>
2023-01-02 17:58:25 +0600

    update

--- src/pages/private/RowProducts/AddCustomeSellReport.js
+++ src/pages/private/RowProducts/AddCustomeSellReport.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 
 export default function AddCustomeSellReport({ stocks }) {
     const [form] = Form.useForm();
-
     const [products, setProducts] = useState();
 
     const columns = [
@@ -30,7 +29,9 @@ export default function AddCustomeSellReport({ stocks }) {
     };
 
     const onFinish = (values) => {
+        console.log(form.getFieldsValue());
 
+        console.log(values);
     }
     return (
         <div className='page-content'>
@@ -127,27 +128,100 @@ export default function AddCustomeSellReport({ stocks }) {
                         <Table columns={columns} dataSource={stocks} pagination={false} />
                     </Col>
                     <Col className="gutter-row" span={8}>
-                        <div className='mb-1'>
-                            <label>Row Product:</label>
-                        </div>
-                        <br />
-                        <div className='mb-1'>
-                            <lable>Quantity</lable>
-                            <Input />
-                        </div>
-                        <br />
-                        <div className='mb-1'>
-                            <lable>Left in Stock: </lable>
-                        </div>
-                        <br />
-                        <div className='mb-1'>
-                            <lable>Cost: </lable>
-                        </div>
+                        <Card>
+                            <div className='mb-1'>
+                                <label>Row Product:</label>
+                            </div>
+                            <br />
+                            <div className='mb-1'>
+                                <lable>Quantity</lable>
+                                <Input />
+                            </div>
+                            <br />
+                            <div className='mb-1'>
+                                <lable>Left in Stock: </lable>
+                            </div>
+                            <br />
+                            <div className='mb-1'>
+                                <lable>Cost: </lable>
+                            </div>
+
+                            <Button style={{ float: "right" }} type="primary">Add</Button>
+                        </Card>
                     </Col>
                     <Col className="gutter-row" span={8}>
                         <Table columns={columns} dataSource={stocks} pagination={false} />
                     </Col>
                 </Row>
+            </Card>
+
+            <Card>
+                <Form layout='vertical'  onFinish={onFinish}>
+                    <Row gutter={16}>
+                        <Col className="gutter-row" span={8}>
+                            <Form.Item
+                                label="Row Product Code"
+                                name="p_code"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input Product Code!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Production Cost"
+                                name="p_code"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input Production Code!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+
+                        <Col className="gutter-row" span={8}>
+                            <Form.Item
+                                label="Total Code"
+                                name="t_code"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input Total Code!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                label="Sell Value"
+                                name="sell_value"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input sell value!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+
+                        <Col className="gutter-row" style={{
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                            alignContent: "center",
+                            flexDirection: "column"
+                        }} span={8}>
+                            <Button type="primary" htmlType="submit" size='large'>Save</Button>
+                        </Col>
+                    </Row>
+                </Form>
             </Card>
         </div>
     )
